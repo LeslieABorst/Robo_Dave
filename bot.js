4563e0d4b6275349387007c230753926d3b9a53c
Leslie A Borst <leslieaborst@gmail.com>
2021-03-09 17:19:32 -0500

    to remove noti role

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', (msg) => {
     }
   });
 
-// Adding reaction-role function
+// Adding Pronoun Roles
 client.on('messageReactionAdd', async (reaction, user) => {
     if (reaction.message.partial) await reaction.message.fetch();
     if (reaction.partial) await reaction.fetch();
@@ -65,49 +65,65 @@ client.on('messageReactionAdd', async (reaction, user) => {
     } else return;
 });
 
-// Removing reaction roles
+// Removing Pronoun Roles
 client.on('messageReactionRemove', async (reaction, user) => {
-    if (reaction.message.partial) await reaction.message.fetch();
-    if (reaction.partial) await reaction.fetch();
-    if (user.bot) return;
-    if (!reaction.message.guild) return;
-    if (reaction.message.channel.id == '777273911267688452') {
-      console.log(reaction.emoji.name);
-      //he him
-       if (reaction.emoji.name === '❤️') {
-         await reaction.message.guild.members.cache
-           .get(user.id)
-           .roles.remove('814177516566741053');
-       }
-       //she her
-       if (reaction.emoji.name === '🧡') {
-         await reaction.message.guild.members.cache
-           .get(user.id)
-           .roles.remove('814178155362910239');
-       }
-       //they them
-       if (reaction.emoji.name === '💛') {
-         await reaction.message.guild.members.cache
-           .get(user.id)
-           .roles.remove('814178009774293038');
-       }
-       //she they
-       if (reaction.emoji.name === '💚') {
-         await reaction.message.guild.members.cache
-           .get(user.id)
-           .roles.remove('814178202724991037');
-       }
-       //he they
-       if (reaction.emoji.name === '💙') {
-         await reaction.message.guild.members.cache
-           .get(user.id)
-           .roles.remove('814178123624873984');
-       }
-       //she he they
-       if (reaction.emoji.name === '💜') {
-         await reaction.message.guild.members.cache
-           .get(user.id)
-           .roles.remove('814178062291566649');
-       }
-   } else return;
-  });
+  if (reaction.message.partial) await reaction.message.fetch();
+  if (reaction.partial) await reaction.fetch();
+  if (user.bot) return;
+  if (!reaction.message.guild) return;
+  if (reaction.message.channel.id == '777273911267688452') {
+    console.log(reaction.emoji.name);
+    //he him
+      if (reaction.emoji.name === '❤️') {
+        await reaction.message.guild.members.cache
+          .get(user.id)
+          .roles.remove('814177516566741053');
+      }
+      //she her
+      if (reaction.emoji.name === '🧡') {
+        await reaction.message.guild.members.cache
+          .get(user.id)
+          .roles.remove('814178155362910239');
+      }
+      //they them
+      if (reaction.emoji.name === '💛') {
+        await reaction.message.guild.members.cache
+          .get(user.id)
+          .roles.remove('814178009774293038');
+      }
+      //she they
+      if (reaction.emoji.name === '💚') {
+        await reaction.message.guild.members.cache
+          .get(user.id)
+          .roles.remove('814178202724991037');
+      }
+      //he they
+      if (reaction.emoji.name === '💙') {
+        await reaction.message.guild.members.cache
+          .get(user.id)
+          .roles.remove('814178123624873984');
+      }
+      //she he they
+      if (reaction.emoji.name === '💜') {
+        await reaction.message.guild.members.cache
+          .get(user.id)
+          .roles.remove('814178062291566649');
+      }
+  } else return;
+});
+
+//Removing Notifications Role
+client.on('messageReactionRemove', async (reaction, user) => {
+  if (reaction.message.partial) await reaction.message.fetch();
+  if (reaction.partial) await reaction.fetch();
+  if (user.bot) return;
+  if (!reaction.message.guild) return;
+  if (reaction.message.channel.id == '802209416685944862') {
+    if (reaction.emoji.name === '814615592233467904') {
+      await reaction.message.guild.members.cache
+        .get(user.id)
+        .roles.remove('663123260753313798');
+    }
+
+  } else return;
+});
